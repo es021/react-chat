2d56743d9ac02fe32cda7469800d671e1f5d1e98
es021 <zulsarhan.shaari@gmail.com>
2017-09-21 14:33:24 +0800

    add style

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,17 @@ import Chat from './chat.js';
 class App extends React.Component {
     render() {
         return(<div>
-            <Chat />
+            <div className="app">
+                <header>This is header</header>
+                <content>
+                    <Chat />
+                </content>
+                <footer>This is footer</footer>
+            </div>
         </div>);
     }
 }
+;
 
 module.exports = App;
 
